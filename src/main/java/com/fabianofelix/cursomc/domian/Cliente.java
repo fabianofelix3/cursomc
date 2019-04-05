0065298c5ec37e4120bd3f37820e62743b07bb59
fabianofelix3 <fabianofelix3@gmail.com>
2019-04-05 17:41:55 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/fabianofelix/cursomc/domian/Cliente.java
+++ src/main/java/com/fabianofelix/cursomc/domian/Cliente.java
@@ -12,11 +12,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.fabianofelix.cursomc.domian.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable {
@@ -30,6 +29,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
