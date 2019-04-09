eb85f210db42e4ed2cc071dfb1d18fb6ac09293d
fabianofelix3 <fabianofelix3@gmail.com>
2019-04-09 18:33:22 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/fabianofelix/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabianofelix/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabianofelix.cursomc.domian.Categoria;
+import com.fabianofelix.cursomc.dto.CategoriaDTO;
 import com.fabianofelix.cursomc.repositories.CategoriaRepository;
 import com.fabianofelix.cursomc.services.exceptions.DataIntegrityException;
 import com.fabianofelix.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
