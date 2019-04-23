05b545dd073ca4252e366e8314756c0b18211222
fabianofelix3 <fabianofelix3@gmail.com>
2019-04-22 23:41:06 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/fabianofelix/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/fabianofelix/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.fabianofelix.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fabianofelix.cursomc.domian.Categoria;
 import com.fabianofelix.cursomc.domian.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
